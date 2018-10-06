9ceede1b96b4fb8345b7c393403c3994a2af8387
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-10-06 07:02:18 +0530

    Create Fibonacci.cpp

---
+++ Fibonacci.cpp
@@ -0,0 +1,30 @@
+//https://www.lintcode.com/problem/fibonacci/description
+
+class Solution {
+public:
+    /**
+     * @param n: an integer
+     * @return: an ineger f(n)
+     */
+    int fibonacci(int n) {
+        // write your code here
+        if(n==1)
+            return 0;
+        if(n==2)
+            return 1;
+            
+        int x1=0;
+        int x2=1;
+        int temp;
+        
+        
+        for(int i=3;i<=n;i++){
+            temp=x1;
+            x1=x2;
+            x2+=temp;
+        }
+        
+        
+        return x2;
+    }
+};
