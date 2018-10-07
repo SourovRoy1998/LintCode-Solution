556f0c901d00ec15899811fa23aab05678bccb7a
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-10-07 07:21:03 +0530

    Create swap_two_integers.cpp

---
+++ swap_two_integers.cpp
@@ -0,0 +1,17 @@
+//https://www.lintcode.com/problem/swap-two-integers-in-array/description
+
+class Solution {
+public:
+    /**
+     * @param A: An integer array
+     * @param index1: the first index
+     * @param index2: the second index
+     * @return: nothing
+     */
+    void swapIntegers(vector<int> &A, int index1, int index2) {
+        // write your code here
+        int temp=A[index1];
+        A[index1]=A[index2];
+        A[index2]=temp;
+    }
+};
