ed69afe33aefe178a8c7199be0aab229716f1f3a
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2018-10-17 20:46:08 +0530

    Create sort_integers.cpp

---
+++ sort_integers.cpp
@@ -0,0 +1,13 @@
+//https://www.lintcode.com/problem/sort-integers/description
+
+class Solution {
+public:
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    void sortIntegers(vector<int> &A) {
+        // write your code here
+        sort(A.begin(),A.end());
+    }
+};
